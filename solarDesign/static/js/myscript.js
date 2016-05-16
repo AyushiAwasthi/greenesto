834a5926d812ce03f8bb47715ced1d8e45aa7356
vipin14119 <vipin14119@iiitd.ac.in>
2016-05-16 14:42:00 +0530

    line graphs and donuts are showing

--- solarDesign/static/js/myscript.js
+++ solarDesign/static/js/myscript.js
@@ -36,12 +36,74 @@ $(document).ready(function() {
 
 });
 
-function validateForm() {
-    var x = document.forms['form']['pincode'].value;
-    alert("Hello");
-    if (x == null || x == "") {
-        alert("Name must be filled out");
-        return false;
+////////////////////////  Saving Form Validating Functions /////////////////////////
+
+function validBill() {
+        var bill = document.forms['form']['bill'].value;
+        if (isNaN(bill) || bill == null){
+            $('#bill').css('border-color','#d50000');
+            $('#bill').css('box-shadow', '0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px #d50000');
+        }
+        else{
+            $('#bill').css('border-color','transparent');
+            $('#bill').css('box-shadow', 'none');
+        }
+    }
+    function validPincode() {
+        var pincode = document.forms['form']['pincode'].value;
+        if (isNaN(pincode)){
+            $('#pincode').css('border-color','#d50000');
+            $('#pincode').css('box-shadow', '0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px #d50000');
+        }
+        else{
+            $('#pincode').css('border-color','transparent');
+            $('#pincode').css('box-shadow', 'none');
+        }
+    }
+    function validConnectionLoad() {
+        var connection_load = document.forms['form']['connection_load'].value;
+        if (isNaN(connection_load)){
+            $('#connection_load').css('border-color','#d50000');
+            $('#connection_load').css('box-shadow', '0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px #d50000');
+        }
+        else{
+            $('#connection_load').css('border-color','transparent');
+            $('#connection_load').css('box-shadow', 'none');
+        }
+    }
+    function validArea() {
+        var roof_area = document.forms['form']['roof_area'].value;
+        if (isNaN(roof_area)){
+            $('#roof_area').css('border-color','#d50000');
+            $('#roof_area').css('box-shadow', '0 1px 1px rgba(0, 0, 0, 0.075) inset, 0 0 8px #d50000');
+        }
+        else{
+            $('#roof_area').css('border-color','transparent');
+            $('#roof_area').css('box-shadow', 'none');
+        }
     }
 
-}
+////////////////////////////// Charts ////////////////////////////////////////
+new Morris.Line({
+    element: 'billChart',
+    data: [
+        {year: '2008', a: 20,b: 0},
+        {year: '2009', a: 10,b: 10},
+        {year: '2010', a: 30,b: 20},
+        {year: '2011', a: 20,b: 10},
+        {year: '2012', a: 40,b: 30}
+    ],
+    xkey: 'year',
+    ykeys: ['a','b'],
+    labels: ['Series A', 'Series B'],
+    lineColors: ['#e65100','#311b92']
+});
+
+new Morris.Donut({
+  element: 'Chart3',
+  data: [
+    {label: "Share 1", value: 12},
+    {label: "Share 2", value: 30}
+  ],
+    colors: ['#e65100','#311b92']
+});
